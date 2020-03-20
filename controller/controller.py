4f4e7408b228f21fdfeec771b97ace4b42244235
Zhehao Tao <zhehaotao@gmail.com>
2020-03-20 15:21:12 +0800

    clean the code

--- controller/controller.py
+++ controller/controller.py
@@ -35,12 +35,6 @@ class SimpleSwitch13(app_manager.RyuApp):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        # install table-miss flow entry
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
@@ -56,18 +50,18 @@ class SimpleSwitch13(app_manager.RyuApp):
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    def handle_arp(self,msg,datapath,packet,etherFrame,inPort):
-        arpPacket = packet.get_protocol(arp)
-        src_IP = arpPacket.src_ip
-        dst_IP = arpPacket.dst_ip
-        src = etherFrame.src
+    def handle_arp(self,msg,datapath,pkt,eth,in_port):
+        arp_packet = pkt.get_protocol(arp)
+        src_IP = arp_packet.src_ip
+        dst_IP = arp_packet.dst_ip
+        src = eth.src
         self.arpTable[src_IP] = src
-        self.mac_to_port[datapath.id][etherFrame.src] = inPort
+        self.mac_to_port[datapath.id][eth.src] = in_port
         # 如果控制器收到ARP请求，则将模拟网关下发ARP回复
-        if arpPacket.opcode == 1:
+        if arp_packet.opcode == 1:
             # 如果ARP请求的目的ip为网关ip，说明是跨网段通信，由控制器下发ARP回复
             if dst_IP in self.domain.values():
-                self.reply_arp(datapath,etherFrame,arpPacket,dst_IP,inPort)
+                self.reply_arp(datapath,eth,arp_packet,dst_IP,in_port)
             # 如果ARP请求的目的ip不是网关ip，说明是同网段通信
             else:
                 ofproto = datapath.ofproto
@@ -75,7 +69,7 @@ class SimpleSwitch13(app_manager.RyuApp):
                 outPort = ofproto.OFPP_FLOOD
                 self.send_arp(datapath, 1, src, src_IP, dstMac, dst_IP, outPort)
         # 如果控制器收到ARP回复，则将其存入ARP缓存
-        elif arpPacket.opcode == 2:
+        elif arp_packet.opcode == 2:
             self.arpTable[src_IP] = src
             # self.logger.info(self.arpTable)
             # 如果ARP回复的目的ip在ARP缓存中，即此目的ip为某一主机的ip
@@ -83,7 +77,7 @@ class SimpleSwitch13(app_manager.RyuApp):
             if dst_IP in self.arpTable:
                 parser = datapath.ofproto_parser
                 ofproto = datapath.ofproto
-                out_port = self.mac_to_port[datapath.id][etherFrame.dst]
+                out_port = self.mac_to_port[datapath.id][eth.dst]
                 actions = [parser.OFPActionOutput(port=out_port)]
                 in_port = msg.match['in_port']
                 out = parser.OFPPacketOut(datapath = datapath,
@@ -93,13 +87,13 @@ class SimpleSwitch13(app_manager.RyuApp):
                                         data = msg.data)
                 # self.logger.info('packet_out:--> %s'%out)
                 datapath.send_msg(out)
-                match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_ARP, eth_dst=etherFrame.dst, eth_src=etherFrame.src)
+                match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_ARP, eth_dst=eth.dst, eth_src=eth.src)
                 self.add_flow(datapath, 1, match, actions)
 
-    def reply_arp(self, datapath, etherFrame, arpPacket, dst_IP, inPort):
-        dstIp = arpPacket.src_ip
-        srcIp = arpPacket.dst_ip
-        dstMac = etherFrame.src
+    def reply_arp(self, datapath, eth, arp_packet, dst_IP, in_port):
+        dstIp = arp_packet.src_ip
+        srcIp = arp_packet.dst_ip
+        dstMac = eth.src
         # 控制器根据收到ARP请求的网关IP来构造对应ARP回复
         if dst_IP in self.domain.values():
             port = list(self.domain.keys())[list(self.domain.values()).index(dst_IP)]
@@ -149,9 +143,9 @@ class SimpleSwitch13(app_manager.RyuApp):
         actions = []
         self.mac_to_port.setdefault(dpid, {})
         # self.logger.info(self.arpTable)
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
+        # if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+        #     # ignore lldp packet
+        #     return
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
         if eth.ethertype == ether_types.ETH_TYPE_ARP:
@@ -159,48 +153,17 @@ class SimpleSwitch13(app_manager.RyuApp):
             self.handle_arp(msg,datapath,pkt,eth,in_port)
         #learn mac to ip
         if eth.ethertype == ether_types.ETH_TYPE_IP:
-            ipv4_pak = pkt.get_protocol(ipv4.ipv4)
-            icmp_pak = pkt.get_protocol(icmp.icmp)
-            dst_ip = ipv4_pak.dst
-            src_ip = ipv4_pak.src
-            self.logger.info("ICMP src:%s dst:%s src_IP:%s dst_IP:%s", src, dst, src_ip, dst_ip)
-            # 若ARP缓存中存在目的IP，则直接找出目的MAC
-            if dst_ip in self.arpTable:
-                dst_mac = self.arpTable[dst_ip]
-                out_port = self.mac_to_port[dpid][dst_mac]
-                # 如果是跨网段转发，则需重新封装二层源目地址
-                if IP(src_ip).make_net('255.255.255.0') != IP(dst_ip).make_net('255.255.255.0'):
-                    actions.append( parser.OFPActionSetField(eth_src=self.hostIp_to_portMac[dst_ip]) )
-                    actions.append( parser.OFPActionSetField(eth_dst=dst_mac) )
-                # 如果不是跨网段转发，则直接转发至相应端口
-                actions.append(parser.OFPActionOutput(port=out_port))
-                out = parser.OFPPacketOut(datapath = datapath,
-                                          buffer_id = ofproto.OFP_NO_BUFFER,
-                                          in_port = in_port,
-                                          actions = actions,
-                                          data = msg.data)
-                # self.logger.info('packet_out:--> %s'%out)
-                datapath.send_msg(out)
-                match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, eth_dst=dst, eth_src=src)
-                self.add_flow(datapath, 1, match, actions)
-            # 若ARP缓存中存在目的IP，则以特殊源地址伪造ARP请求
-            else:
-                srcMac = "00:00:00:00:00:88"
-                srcIp = "88.88.88.88"
-                dstMac = "ff:ff:ff:ff:ff:ff"
-                dstIp = dst_ip
-                outPort = ofproto.OFPP_FLOOD
-                self.send_arp(datapath, 1, srcMac, srcIp, dstMac, dstIp, outPort)
-                return
+            self.handle_ip(msg,datapath,pkt,eth,in_port)            
+            
         # topo发现主机，及边缘网络的端口，并配置ip
-
         hosts = get_host(self)
         for host in hosts:
-            print(host.to_dict())
+            # print(host.to_dict())
             try:                
                 host_ip = host.ipv4[0]
             except:
-                print ("Please wait for some time")
+                pass
+                # print ("Controller is finding host")
             else:
                 self.hostIp_to_portMac[host_ip] = host.port.hw_addr
                 temp = host_ip.split(".")
@@ -210,3 +173,43 @@ class SimpleSwitch13(app_manager.RyuApp):
                 gateway_ip += '10'
                 # self.gateway.add(host.port)
                 self.domain[host.port] = gateway_ip
+    
+    def handle_ip(self,msg,datapath,pkt,eth,in_port):
+        dpid = datapath.id
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        ipv4_pak = pkt.get_protocol(ipv4.ipv4)
+        icmp_pak = pkt.get_protocol(icmp.icmp)
+        dst = eth.dst
+        src = eth.src
+        dst_ip = ipv4_pak.dst
+        src_ip = ipv4_pak.src
+        actions = []
+        # 若ARP缓存中存在目的IP，则直接找出目的MAC
+        if dst_ip in self.arpTable:
+            dst_mac = self.arpTable[dst_ip]
+            out_port = self.mac_to_port[dpid][dst_mac]
+            # 如果是跨网段转发，则需重新封装二层源目地址
+            if IP(src_ip).make_net('255.255.255.0') != IP(dst_ip).make_net('255.255.255.0'):
+                actions.append( parser.OFPActionSetField(eth_src=self.hostIp_to_portMac[dst_ip]) )
+                actions.append( parser.OFPActionSetField(eth_dst=dst_mac) )
+            # 如果不是跨网段转发，则直接转发至相应端口
+            actions.append(parser.OFPActionOutput(port=out_port))
+            out = parser.OFPPacketOut(datapath = datapath,
+                                      buffer_id = ofproto.OFP_NO_BUFFER,
+                                      in_port = in_port,
+                                      actions = actions,
+                                      data = msg.data)
+            # self.logger.info('packet_out:--> %s'%out)
+            datapath.send_msg(out)
+            match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, eth_dst=dst, eth_src=src)
+            self.add_flow(datapath, 1, match, actions)
+        # 若ARP缓存中存在目的IP，则以特殊源地址伪造ARP请求
+        else:
+            srcMac = "00:00:00:00:00:88"
+            srcIp = "88.88.88.88"
+            dstMac = "ff:ff:ff:ff:ff:ff"
+            dstIp = dst_ip
+            outPort = ofproto.OFPP_FLOOD
+            self.send_arp(datapath, 1, srcMac, srcIp, dstMac, dstIp, outPort)
+            return
