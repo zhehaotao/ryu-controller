2fad402fc7dd35d3067bea67a4c35dfdadf854fc
Zhehao Tao <zhehaotao@gmail.com>
2020-05-07 23:40:01 +0800

    added test topo

---
+++ topo/same3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.node import OVSSwitch, Controller, RemoteController
+
+class SingleSwitchTopo(Topo):
+    "Single switch connected to 2 hosts in the same network."
+    def build(self):
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+	s3 = self.addSwitch('s3')
+        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="192.168.1.1/24")
+        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="192.168.1.2/24")
+        self.addLink(h1, s1)
+        self.addLink(s1, s2)
+	self.addLink(s2, s3)
+        self.addLink(s3, h2)
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo = SingleSwitchTopo()
+    c1 = RemoteController('c1', ip='127.0.0.1')
+    net = Mininet(topo=topo, controller=c1)
+    net.start()
+    CLI(net)
+
